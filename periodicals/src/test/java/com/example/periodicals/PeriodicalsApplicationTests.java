b08c7cacc0cb7418c30a76ed26003ffa125e6a91
Denys-Drabskyi <114095588+Denys-Drabskyi@users.noreply.github.com>
2022-11-16 18:14:51 +0200

    Add files via upload

---
+++ periodicals/src/test/java/com/example/periodicals/PeriodicalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.periodicals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodicalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
